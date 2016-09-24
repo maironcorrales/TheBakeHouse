3c7effc93a8ae05a29f7e034f81f14b976a92f34
danicormu <danicormu@gmail.com>
2016-09-24 16:04:41 -0600

    Some Things

---
+++ The Bakehouse/The Bakehouse/Domain/Administrator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace The_Bakehouse.Domain
+{
+    public class Administrator
+    {
+        private int idAdmin;
+        private string username;
+        private string password;        
+
+        //Constructor
+        public Administrator(int id, string username, string password)
+        {
+            this.idAdmin = id;
+            this.username = username;
+            this.password = password;
+        }
+        
+        //Constructor
+        public Administrator(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        //Setters and Getters
+        public int Id
+        {
+            get { return idAdmin; }
+            set { idAdmin = value; }
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
---
+++ The Bakehouse/The Bakehouse/Domain/Subscriber.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace The_Bakehouse.Domain
+{
+    public class Subscriber
+    {
+        private int id;       
+        private string name;       
+        private string code;       
+        private string email;        
+        private int telephone;        
+
+        public Subscriber(int id, string name, string code, string email, int telephone)
+        {
+            this.id = id;
+            this.name = name;
+            this.code = code;
+            this.email = email;
+            this.telephone = telephone;
+        }
+
+        public Subscriber(string name, string code, string email, int telephone)
+        {
+            this.name = name;
+            this.code = code;
+            this.email = email;
+            this.telephone = telephone;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public int Telephone
+        {
+            get { return telephone; }
+            set { telephone = value; }
+        }
+    }
+}
