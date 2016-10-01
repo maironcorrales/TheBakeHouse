fbd693fc3d831d9425c032d4eda6ab32054c4bd9
danicormu <danicormu@gmail.com>
2016-09-30 21:16:03 -0600

    Data Layer Ready

---
+++ The Bakehouse/The Bakehouse/Data/BillData.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using The_Bakehouse.Domain;
+using MySql.Data.MySqlClient;
+
+namespace The_Bakehouse.Data
+{
+    public class BillData:DBConnection
+    {
+        //get all the bills
+        public List<Bill> getAllBill()
+        {
+            List<Bill> list = new List<Bill>();
+            try
+            {
+                ConnectDB();
+                MySqlCommand query = new MySqlCommand("select * from factura;", Conn);
+                Conn.Open();
+                MySqlDataReader reader = query.ExecuteReader();
+                while (reader.Read())
+                {
+                    Bill bill = new Bill(reader.GetInt32(0), reader.GetDouble(1), reader.GetString(2));
+                    list.Add(bill);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                disconnectDB();
+                Conn.Close();
+            }
+            return list;
+        }
+
+        //add a bill
+        public bool addBill(Bill bill)
+        {
+            bool flag = false;
+            try
+            {
+                ConnectDB();
+                MySqlCommand query = new MySqlCommand("insert into factura values(@amount, @date);", Conn);
+                query.Parameters.AddWithValue("@amount", bill.Amount);
+                query.Parameters.AddWithValue("@date", bill.Date);
+                Conn.Open();
+                query.ExecuteNonQuery();
+                flag = true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                disconnectDB();
+                Conn.Close();
+            }
+            return flag;
+        }
+        
+        //Se puede actualizar la factura?
+        //update the bill
+        public bool updateBill(Bill bill)
+        {
+            bool flag = false;
+            try
+            {
+                ConnectDB();
+                MySqlCommand query = new MySqlCommand("update factura set Monto = @amount, Fecha = @date;", Conn);
+                query.Parameters.AddWithValue("@amount", bill.Amount);
+                query.Parameters.AddWithValue("@date", bill.Date);
+                Conn.Open();
+                query.ExecuteNonQuery();
+                flag = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                disconnectDB();
+                Conn.Close();
+            }
+            return flag;
+        }
+
+        //delete bill 
+        public bool deleteBill(int id)
+        {
+            bool flag = false;
+            try
+            {
+                ConnectDB();
+                MySqlCommand query = new MySqlCommand("delete from factura where idFactura = @id;", Conn);
+                query.Parameters.AddWithValue("@id", id);
+                Conn.Open();
+                query.ExecuteNonQuery();
+                flag = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                disconnectDB();
+                Conn.Close();
+            }
+            return flag;
+        }
+    }
+}
---
+++ The Bakehouse/The Bakehouse/Data/NotificationData.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using The_Bakehouse.Domain;
+using MySql.Data.MySqlClient;
+
+namespace The_Bakehouse.Data
+{
+    public class NotificationData:DBConnection
+    {
+        //Get all Notification
+        public List<Notification> getAllNotification()
+        {
+            List<Notification> list = new List<Notification>();
+            try
+            {
+                ConnectDB();
+                MySqlCommand query = new MySqlCommand("select * from notificaciones;", Conn);
+                Conn.Open();
+                MySqlDataReader reader = query.ExecuteReader();
+                while (reader.Read())
+                {
+                    Notification notification = new Notification(reader.GetInt32(0), reader.GetString(1), reader.GetBoolean(3));
+                    list.Add(notification);
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                disconnectDB();
+                Conn.Close();
+            }
+            return list;
+        }
+
+        //add Notif
+        public bool addNotification(Notification notification)
+        {
+            bool flag = false;
+            try
+            {
+                ConnectDB();
+                MySqlCommand query = new MySqlCommand("insert into notification values(@notification, @seen);", Conn);
+                query.Parameters.AddWithValue("@notification", notification.Notificate);
+                query.Parameters.AddWithValue("@seen", notification.Seen);
+                Conn.Open();
+                query.ExecuteNonQuery();
+                flag = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                disconnectDB();
+                Conn.Close();
+            }
+            return flag;
+        }
+
+        //update Notif
+        public bool updateNotification(Notification notification)
+        {
+            bool flag = false;
+            try
+            {
+                ConnectDB();
+                MySqlCommand query = new MySqlCommand("update notificaciones set notificacion = @notification, visto = @seen;", Conn);
+                query.Parameters.AddWithValue("@notification", notification.Notificate);
+                query.Parameters.AddWithValue("@seen", notification.Seen);
+                Conn.Open();
+                query.ExecuteNonQuery();
+                flag = true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                disconnectDB();
+                Conn.Close();
+            }
+            return flag;
+        }
+
+        //delete Notif
+        public bool deleteNotification(int idNot)
+        {
+            bool flag = false;
+            try
+            {
+                ConnectDB();
+                MySqlCommand query = new MySqlCommand("delete from notificaciones where idNotificacion = id;", Conn);
+                query.Parameters.AddWithValue("@id", idNot);
+                Conn.Open();
+                query.ExecuteNonQuery();
+                flag = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                disconnectDB();
+                Conn.Close();
+            }
+            return flag;
+        }
+    }
+}
